43043a0599655ed2b49c2ba0421da6fa83672395
Daniel Karlkvist <danielkarlkvist@gmail.com>
2020-04-22 21:46:41 +0200

    added xcode project

---
+++ Chalmers Menu/Chalmers MenuTests/Chalmers_MenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chalmers_MenuTests.swift
+//  Chalmers MenuTests
+//
+//  Created by Daniel Karlkvist on 2020-04-22.
+//  Copyright © 2020 Daniel Karlkvist. All rights reserved.
+//
+
+import XCTest
+@testable import Chalmers_Menu
+
+class Chalmers_MenuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
